40132c56f3d8a241d28cb53ad54cb5d787bd4cab
Sara <saramihajlovic94@gmail.com>
2024-08-24 14:16:30 +0200

    feat: add authorization

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using AccommodationService.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.AddAppAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
