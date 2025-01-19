bd4314a6d7921099fc22eac459150342c8b5ce52
cojic <102799668+cojic@users.noreply.github.com>
2025-01-19 21:16:51 +0100

    feat: cicd pipeline

--- AccommodationService/Program.cs
+++ AccommodationService/Program.cs
@@ -1,12 +1,28 @@
 using AccommodationService.Data;
 using AccommodationService.Extensions;
+using AccommodationService.Filters;
+using AccommodationService.Middlewares;
 using AccommodationService.Model.Entity;
 using AccommodationService.Model.Settings;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+if (environment == "Docker")
+{
+    builder.Configuration.AddJsonFile(
+        "appsettings.Docker.json",
+        optional: true,
+        reloadOnChange: true
+    );
+}
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -14,8 +30,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCustomCors();
 
-//builder.Services.AddKafkaServices(builder.Configuration);
+builder.Services.AddKafkaServices(builder.Configuration);
 builder.Services.AddCustomServices();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
 builder.Services.Configure<ApiBehaviorOptions>(options =>
@@ -26,7 +43,10 @@ builder.Services.AddIdentity<User, IdentityRole<int>>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ValidationFilterAttribute>();
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
@@ -57,14 +77,27 @@ using (var scope = app.Services.CreateScope())
 app.UseCors(CorsExtensions.GetCorsPolicyName());
 
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Docker")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseExceptionHandler(builder =>
+{
+    builder.Run(async context =>
+    {
+        var exceptionHandler = context.RequestServices.GetRequiredService<IExceptionHandler>();
+        var exception = context.Features.Get<IExceptionHandlerPathFeature>()?.Error;
 
+        if (exception != null)
+        {
+            await exceptionHandler.TryHandleAsync(context, exception, context.RequestAborted);
+        }
+    });
+});
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
