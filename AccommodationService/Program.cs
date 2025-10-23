529c99d21a239e8ad54643f021dfc7118de4929d
sara <sara.mihajlovic@biosense.rs>
2025-10-24 01:30:23 +0200

    feature: monitoring

--- AccommodationService/Program.cs
+++ AccommodationService/Program.cs
@@ -5,12 +5,16 @@ using AccommodationService.Middlewares;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Diagnostics.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var serviceName = Environment.GetEnvironmentVariable("SERVICE_NAME") ?? "unknown-service";
+
 if (environment == "Docker")
 {
     builder.Configuration.AddJsonFile(
@@ -18,6 +22,8 @@ if (environment == "Docker")
         optional: true,
         reloadOnChange: true
     );
+    builder.AddMonitoring();
+
 }
 builder.Configuration.AddEnvironmentVariables();
 
@@ -60,6 +66,50 @@ builder.WebHost.ConfigureKestrel(options =>
 });
 
 var app = builder.Build();
+
+
+var meter = new Meter("custom_metrics_"+serviceName, "1.0");
+var responseSizeCounter = meter.CreateCounter<long>("http_response_size_bytes");
+app.Use(async (context, next) =>
+{
+    var originalBodyStream = context.Response.Body;
+    using var memoryStream = new MemoryStream();
+    context.Response.Body = memoryStream;
+
+    await next();
+
+    var size = memoryStream.Length;
+    responseSizeCounter.Add(size, new KeyValuePair<string, object?>("service_name", serviceName));
+
+    memoryStream.Seek(0, SeekOrigin.Begin);
+    await memoryStream.CopyToAsync(originalBodyStream);
+    context.Response.Body = originalBodyStream;
+});
+var uniqueVisitorCounter = meter.CreateCounter<long>("unique_visitors");
+
+app.Use(async (context, next) =>
+{
+    await next();
+
+    var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+    var browser = context.Request.Headers["User-Agent"].ToString() ?? "unknown";
+    var timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm"); // group by minute
+
+    // Use tags as labels
+    uniqueVisitorCounter.Add(1, new KeyValuePair<string, object?>("ip", ip),
+                               new KeyValuePair<string, object?>("browser", browser),
+                               new KeyValuePair<string, object?>("timestamp", timestamp));
+});
+
+if (!app.Environment.IsEnvironment("Testing"))
+{
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.IncludeQueryInRequestPath = true;
+    });
+
+    app.UseRequestResponseLogging();
+}
 
 // Seed data
 using (var scope = app.Services.CreateScope())
